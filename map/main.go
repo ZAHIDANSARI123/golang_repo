3f39090ed0d64cd360634de138d262e08f369556
ZAHIDANSARI123 <zahidrajagp@gmail.com>
2025-08-28 23:52:08 +0530

    update folder map and struct

---
+++ map/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// map creation using make function
+
+	studentGrades := make(map[string]int)
+
+	studentGrades["Alice"] = 100
+	studentGrades["Bob"] = 90
+	studentGrades["Charlie"] = 92
+	studentGrades["Diana"] = 78
+
+	fmt.Println("Mark of Alice is :", studentGrades["Alice"])
+	studentGrades["Alice"] = 88
+	fmt.Println("Updated Mark of Alice is :", studentGrades["Alice"])
+
+	delete(studentGrades, "Alice")
+	fmt.Println("Mark of Alice is :", studentGrades["Alice"])
+
+
+	// checking	 if a key exists
+	Grades, Exists := studentGrades["Alice"]
+	fmt.Println("Mark of Alice is :", Grades)
+	fmt.Println("Is Alice's mark present in the map?", Exists)	
+
+
+
+	grades, exists := studentGrades["Charlie"]
+	fmt.Println("Mark of Charlie is :", grades)
+	fmt.Println("Is Charlie's mark present in the map?", exists)	
+
+
+	// range loop to iterate over map unordered collection
+	for index, value := range studentGrades {
+		fmt.Printf("key is %s and marks is %d\n", index, value)
+	}
+
+	// map creation using map literal
+	person := map[string]int{
+		"John":  25,
+		"Jane":  30,
+		"Emily": 22,
+	}
+
+	for index, value := range person {
+		fmt.Printf("------key is %s and marks is %d\n", index, value)
+	}
+
+}
---
+++ struct/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age  int
+}
+
+type Contact struct {
+	Email   string
+	Address string
+	Phone   string
+}
+
+
+type Address struct {
+	City    string
+	Country string
+	ZipCode string
+}
+
+type Employee struct {
+	Person_Details  Person
+Person_Contact Contact
+	Address_Details Address
+}
+
+func main() {
+	var jahid Person
+	fmt.Println("Jahid person :", jahid)
+	jahid.FirstName = "Jahid"
+	jahid.LastName = "Hossain"	
+	jahid.Age = 33
+	fmt.Println("Jahid person : ", jahid)	
+
+
+	user := Person{
+		FirstName: "John",
+		LastName:  "Doe",
+		Age: 30,
+	}
+	fmt.Println("User person : ", user)
+
+	var user2 = new(Person)
+	user2.FirstName = "Jane"
+	user2.LastName = "Smith"
+	user2.Age = 28
+	fmt.Println("User2 person : ", user2)
+
+
+
+	var Employee1 Employee
+	Employee1.Person_Details = Person{
+		FirstName: "Jahid",
+		LastName:  "Hossain",
+		Age: 33,
+	}
+	Employee1.Person_Contact.Email = "jahid@gmail.com"
+	Employee1.Person_Contact.Phone = "01700000000"
+
+	Employee1.Address_Details = Address{
+		City:    "Dhaka",
+		Country: "Bangladesh",	
+		ZipCode: "1207",
+	}
+
+	fmt.Println("Employee1 : ", Employee1)
+}
