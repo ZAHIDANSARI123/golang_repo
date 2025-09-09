ab7d03889e362db5897527c867eec02ed19b495f
ZAHIDANSARI123 <zahidrajagp@gmail.com>
2025-09-09 22:31:30 +0530

    update folder defer

---
+++ defer/main.go
@@ -0,0 +1,11 @@
+package main
+
+import ("fmt")
+
+func main() {
+	fmt.Println("This is the first statement")
+	data := add(5, 10)
+	fmt.Println("sum is :", data)
+	defer fmt.Println("This is the second statement")
+	fmt.Println("This is the third statement")
+}
